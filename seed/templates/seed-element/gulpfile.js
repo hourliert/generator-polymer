2ec77f22cf54af5952ae78784bf54cb4f114cde7
Thomas Hourlier <thomas.hourlier@cnode.fr>
2015-07-12 14:40:32 -0700

    Added tag tasks support in gulpfile.

--- seed/templates/seed-element/gulpfile.js
+++ seed/templates/seed-element/gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp'),
 	bs = require('browser-sync').create(),
 	bg = require("gulp-bg"),
-	ps = require('ps-node');
+	ps = require('ps-node'),
+  git = require('gulp-git'),
+  bump = require('gulp-bump'),
+  tag_version = require('gulp-tag-version'),
+  filter = require('gulp-filter');
 
 var POLYSERVE_PORT = 8080,
     elementName = 'seed-element';
@@ -31,14 +35,14 @@ gulp.task('polyserve', function(cb) {
     if (err) {
       throw new Error(err);
     }
- 
+
     if (!resultList.length) {
       bg('./node_modules/polyserve/bin/polyserve', '-p ' + POLYSERVE_PORT)();
     }
     cb();
-  }); 
+  });
 });
-	
+
 gulp.task('serve', ['polyserve'], function(cb) {
   browserSyncConfig('/components/' + elementName + '/demo/', cb);
   watchComponent();
@@ -55,3 +59,22 @@ gulp.task('test:watch', ['polyserve'], function(cb) {
 });
 
 try { require('web-component-tester').gulp.init(gulp); } catch (err) {}
+
+function inc(importance) {
+  // get all the files to bump version in
+  return gulp.src(['./package.json', './bower.json'])
+    // bump the version number in those files
+    .pipe(bump({type: importance}))
+    // save it back to filesystem
+    .pipe(gulp.dest('./'))
+    // commit the changed version number
+    .pipe(git.commit('bumps package version'))
+    // read only one file to get the version number
+    .pipe(filter('bower.json'))
+    // **tag it in the repository**
+    .pipe(tag_version());
+}
+
+gulp.task('patch', function() { return inc('patch'); });
+gulp.task('feature', function() { return inc('minor'); });
+gulp.task('release', function() { return inc('major'); });
